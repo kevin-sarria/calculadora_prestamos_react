07f994567dbc847ac5315b3e50dc2a096d305c21
kevin-sarria <81386916+kevin-sarria@users.noreply.github.com>
2022-12-02 19:06:56 -0500

    Proyecto cotizador de prestamos en react Terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Button from "./components/Button";
+import { formatearDinero, calcularTotalPagar } from './helpers';
 
 
 function App() {
 
   const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6);
+  const [total, setTotal] = useState(0);
+  const [mensual, setMensual] = useState(0);
+
+  useEffect( () => {
+    const resultado = calcularTotalPagar(cantidad, meses);
+    setTotal(resultado);
+  }, [cantidad, meses] );
+
+  useEffect( () => {
+    // Calcular el pago Mensual
+    setMensual( total / meses );
+  }, [total] );
+
+  const min = 0;
+  const max = 20000;
+  const step = 100;
 
   function handleChange(e) {
-    setCantidad(cantidad + Number(e.target.value))
+    setCantidad( Number(e.target.value))
+  }
+
+  function handleClickDecremento () {
+
+    const valor = cantidad - step;
+
+    if( valor < min ) {
+      return;
+    }
+
+    setCantidad(valor);
+
+  }
+
+  function handleClickIncremento() {
+
+    const valor = cantidad + step;
+
+    if( valor > max ) {
+      return;
+    }
+
+    setCantidad(valor);
+
   }
 
   return (
@@ -15,18 +58,58 @@ function App() {
       
       <Header />
 
+      <div className="flex justify-between my-6">
+        
+        <Button
+          operador='-'
+          fn={handleClickDecremento}
+        />
+
+        <Button 
+          operador='+'
+          fn={handleClickIncremento}
+        />
+
+
+      </div>
+
       <input
         type="range"
         name=""
         id=""
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
         onChange={handleChange}
-        min="0"
-        max="2000"
-        step="500"
+        min={min}
+        max={max}
+        step={step}
+        value={cantidad}
         />
 
-    <p className="text-center my-10 text-5xl font-extrabold text-indigo-500">{cantidad}</p>
+    <p className="text-center my-10 text-5xl font-extrabold text-indigo-500">{formatearDinero(cantidad)}</p>
+
+    <h2 className="text-2xl font-extrabold text-gray-500 text-center">
+      Elige un <span className="text-indigo-600">Plazo</span> a pagar
+    </h2>
+
+
+    <select className="mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500"
+    value={meses}
+    onChange={ e => setMeses(e.target.value) }
+    >
+      <option value="6">6 Meses</option>
+      <option value="12">12 Meses</option>
+      <option value="24">24 Meses</option>
+    </select>
+
+    <div className="my-5 space-y-3 bg-gray-50 p-5">
+      <h2 className="text-2xl font-extrabold text-gray-500 text-center">Resumen <span className="text-indigo-600">de pagos</span></h2>
+
+      <p className="text-xl text-gray-500 text-center font-bold">{meses} Meses</p>
+      <p className="text-xl text-gray-500 text-center font-bold"> { formatearDinero( total ) } Total a Pagar</p>
+      <p className="text-xl text-gray-500 text-center font-bold"> {formatearDinero(mensual)} Mensuales </p>
+
+    </div>
+
 
     </div>
   )
